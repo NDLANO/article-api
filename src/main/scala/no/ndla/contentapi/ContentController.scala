7cfb0926b7ea77fd867e5a495ef0352472ca9318
Kenneth Stigen <kes@knowit.no>
2015-10-27 08:01:24 +0100

    NDLA-53: Endret fra h5p til egen metadata og html

--- src/main/scala/no/ndla/contentapi/ContentController.scala
+++ src/main/scala/no/ndla/contentapi/ContentController.scala
@@ -29,6 +29,16 @@ class ContentController (implicit val swagger:Swagger) extends ScalatraServlet w
       queryParam[Option[String]]("license").description("Return only content with provided license.")
       ))
 
+  val getContentById =
+    (apiOperation[List[ContentMetaInformation]]("getContentById")
+    summary "Show content for a specified Id"
+    notes "Shows the content for the specified id."
+    parameters (
+    headerParam[Option[String]]("X-Correlation-ID").description("User supplied correlation-id. May be omitted."),
+    headerParam[Option[String]]("app-key").description("Your app-key. May be omitted to access api anonymously, but rate limiting applies on anonymous access."),
+    pathParam[String]("content_id").description("Id of the content that is to be returned")
+    ))
+
   before() {
     contentType = formats("json")
     LoggerContext.setCorrelationID(Option(request.getHeader("X-Correlation-ID")))
@@ -47,7 +57,26 @@ class ContentController (implicit val swagger:Swagger) extends ScalatraServlet w
     )
   }
 
-  get("/:content_id") {
-    io.Source.fromInputStream(getClass.getResourceAsStream(s"/testdata/${params("content_id")}.json")).mkString
+  val testdata = Map(
+    "1" ->
+      ContentMetaInformation("1",
+        List(ContentTitle("Myklesaken splittet Norge", Some("nb"))),
+        io.Source.fromInputStream(getClass.getResourceAsStream(s"/testdata/1.html")).mkString,
+        Copyright(License("by-nc-sa", "Creative Commons Attribution-NonCommercial-ShareAlike 2.0 Generic", Some("https://creativecommons.org/licenses/by-nc-sa/2.0/")), "NTB Tema", List(Author("forfatter", "INGRID BRUBAKER"))),
+        List(ContentTag("myklesaken", Some("nb")), ContentTag("norge", Some("nb")))),
+
+    "2" -> ContentMetaInformation("2",
+      List(ContentTitle("HMS Spillet", Some("nb"))),
+      io.Source.fromInputStream(getClass.getResourceAsStream(s"/testdata/2.html")).mkString,
+      Copyright(License("by-nc-sa", "Creative Commons Attribution-NonCommercial-ShareAlike 2.0 Generic", Some("https://creativecommons.org/licenses/by-nc-sa/2.0/")), "Amendor", List(Author("forfatter", "GURI BENTE HÅRBERG"))),
+      List(ContentTag("hms", Some("nb")), ContentTag("spill", Some("nb"))))
+  )
+
+
+  get("/:content_id", operation(getContentById)) {
+    testdata get params("content_id") match {
+      case Some(x) => x
+      case None => None
+    }
   }
 }
--- src/main/scala/no/ndla/contentapi/model/ContentApiModels.scala
+++ src/main/scala/no/ndla/contentapi/model/ContentApiModels.scala
@@ -12,3 +12,44 @@ case class ContentMetaSummary(
   @(ApiModelProperty @field)(description = "The full url to where the complete metainformation about the content can be found") metaUrl:String,
   @(ApiModelProperty @field)(description = "Describes the license of the content") license:String
 )
+
+@ApiModel(description = "Meta information about the content")
+case class ContentMetaInformation(
+  @(ApiModelProperty @field)(description = "The unique id of the content") id:String,
+  @(ApiModelProperty @field)(description = "Available titles for the image") titles:List[ContentTitle],
+  @(ApiModelProperty @field)(description = "The content") content: String,
+  @(ApiModelProperty @field)(description = "Describes the copyright information for the image") copyright:Copyright,
+  @(ApiModelProperty @field)(description = "Searchable tags for the image") tags:List[ContentTag]
+)
+
+@ApiModel(description = "Description of a title")
+case class ContentTitle(
+  @(ApiModelProperty @field)(description = "The freetext title of the image") title:String,
+  @(ApiModelProperty @field)(description = "ISO 639-1 code that represents the language used in title") language:Option[String]
+)
+
+@ApiModel(description = "Description of copyright information")
+case class Copyright(
+  @(ApiModelProperty @field)(description = "Describes the license of the image") license:License,
+  @(ApiModelProperty @field)(description = "Reference to where the image is procured") origin:String,
+  @(ApiModelProperty @field)(description = "List of authors") authors:List[Author]
+)
+
+@ApiModel(description = "Description of the tags of the content")
+case class ContentTag(
+  @(ApiModelProperty @field)(description = "The searchable tag.") tag:String,
+  @(ApiModelProperty @field)(description = "ISO 639-1 code that represents the language used in tag") language:Option[String]
+)
+
+@ApiModel(description = "Description of license information")
+case class License(
+  @(ApiModelProperty @field)(description = "The name of the license") license:String,
+  @(ApiModelProperty @field)(description = "Description of the license") description:String,
+  @(ApiModelProperty @field)(description = "Url to where the license can be found") url:Option[String]
+)
+
+@ApiModel(description = "Information about an author")
+case class Author(
+  @(ApiModelProperty @field)(description = "The description of the author. Eg. Photographer or Supplier") `type`:String,
+  @(ApiModelProperty @field)(description = "The name of the of the author") name:String
+)
