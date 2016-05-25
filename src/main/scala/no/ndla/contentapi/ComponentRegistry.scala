778dc0ecbd5e9ebd8b90322b332a88f9366a0cfb
Henrik Nårstad <henrik.narstad@knowit.no>
2016-05-25 12:04:33 +0200

    NDLA-252: lazy vals i ComponentRegistry

--- src/main/scala/no/ndla/contentapi/ComponentRegistry.scala
+++ src/main/scala/no/ndla/contentapi/ComponentRegistry.scala
@@ -15,7 +15,7 @@ object ComponentRegistry
   with ElasticContentSearchComponent
   with ElasticContentIndexComponent {
 
-  val dataSource = new PGPoolingDataSource()
+  lazy val dataSource = new PGPoolingDataSource()
   dataSource.setUser(ContentApiProperties.get("META_USER_NAME"))
   dataSource.setPassword(ContentApiProperties.get("META_PASSWORD"))
   dataSource.setDatabaseName(ContentApiProperties.get("META_RESOURCE"))
@@ -25,12 +25,12 @@ object ComponentRegistry
   dataSource.setMaxConnections(ContentApiProperties.getInt("META_MAX_CONNECTIONS"))
   dataSource.setCurrentSchema(ContentApiProperties.get("META_SCHEMA"))
 
-  val elasticClient = ElasticClient.remote(
+  lazy val elasticClient = ElasticClient.remote(
     ImmutableSettings.settingsBuilder().put("cluster.name", ContentApiProperties.SearchClusterName).build(),
     ElasticsearchClientUri(s"elasticsearch://${ContentApiProperties.SearchHost}:${ContentApiProperties.SearchPort}")
   )
 
-  val contentRepository = new ContentRepository
-  val elasticContentSearch = new ElasticContentSearch
-  val elasticContentIndex = new ElasticContentIndex
+  lazy val contentRepository = new ContentRepository
+  lazy val elasticContentSearch = new ElasticContentSearch
+  lazy val elasticContentIndex = new ElasticContentIndex
 }
