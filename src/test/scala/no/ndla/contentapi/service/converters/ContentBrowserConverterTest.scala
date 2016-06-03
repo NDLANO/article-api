c9af62556996a4f4142eb647f300cef286570c65
Henrik Nårstad <henrik.narstad@knowit.no>
2016-06-03 14:36:43 +0200

    NDLA-249_new: Test for bildekovertering

--- src/test/scala/no/ndla/contentapi/service/converters/ContentBrowserConverterTest.scala
+++ src/test/scala/no/ndla/contentapi/service/converters/ContentBrowserConverterTest.scala
@@ -1,7 +1,8 @@
 package no.ndla.contentapi.service.converters
 
 import no.ndla.contentapi.TestEnvironment
-import no.ndla.contentapi.model.RequiredLibrary
+import no.ndla.contentapi.model.{Copyright, License, RequiredLibrary}
+import no.ndla.contentapi.service.{Image, ImageMetaInformation, ImageVariants}
 import no.ndla.learningpathapi.UnitSuite
 import org.jsoup.Jsoup
 import org.mockito.Mockito._
@@ -33,5 +34,20 @@ class ContentBrowserConverterTest extends UnitSuite with TestEnvironment {
 
     element.outerHtml().replace("\n", "") should equal (expectedResult)
     requiredLibraries.length should equal (1)
+  }
+
+  test("That Content-browser strings of type image are converted into HTML img tags") {
+    val (nodeId, imageUrl, alt) = ("1234", "full.jpeg", "Fotografi")
+    val initialContent = s"<article><p>[contentbrowser ==nid=${nodeId}==imagecache=Fullbredde==width===alt=${alt}==link===node_link=1==link_type=link_to_content==lightbox_size===remove_fields[76661]=1==remove_fields[76663]=1==remove_fields[76664]=1==remove_fields[76666]=1==insertion===link_title_text= ==link_text= ==text_align===css_class=contentbrowser contentbrowser]</p></article>"
+    val expectedResult = s"""<article> <p><img src="/images/${imageUrl}" alt="${alt}" /></p></article>"""
+    implicit val requiredLibraries = ListBuffer[RequiredLibrary]()
+    val imageMeta = Some(ImageMetaInformation("1", List(), List(), ImageVariants(Some(Image("small.jpeg", 128, "")), Some(Image(imageUrl, 256, ""))), Copyright(License("", "", Some("")), "", List()), List()))
+
+    when(extractService.getNodeType(nodeId)).thenReturn((Some("image")))
+    when(imageApiService.getMetaByExternId(nodeId)).thenReturn(imageMeta)
+    val element = contentBrowserConverter.convert(Jsoup.parseBodyFragment(initialContent).body().child(0))
+
+    element.outerHtml().replace("\n", "") should equal (expectedResult)
+    requiredLibraries.length should equal (0)
   }
 }
