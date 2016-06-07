b1dd102d3556be234f9308e5182d4f4052ebf4c7
Henrik Nårstad <henrik.narstad@knowit.no>
2016-06-07 13:58:52 +0200

    NDLA-290: Konverter div.right til aside, div.hide til details-summary

--- src/main/scala/no/ndla/contentapi/service/converters/SimpleTagConverter.scala
+++ src/main/scala/no/ndla/contentapi/service/converters/SimpleTagConverter.scala
@@ -2,6 +2,7 @@ package no.ndla.contentapi.service.converters
 
 import no.ndla.contentapi.integration.ConverterModule
 import no.ndla.contentapi.model.RequiredLibrary
+import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
 import scala.collection.JavaConversions._
@@ -12,15 +13,11 @@ object SimpleTagConverter extends ConverterModule {
     var elements = el.select("div")
     for (el <- elements) {
       el.className() match {
-        case "full" => el.unwrap()
-        case "paragraph" => {
-          el.tagName("section")
-          el.removeAttr("class")
-        }
-        case "quote" => {
-          el.tagName("blockquote")
-          el.removeAttr("class")
-        }
+        case "right" => replaceTag(el, "aside")
+        case "paragraph" => replaceTag(el, "section")
+        case "quote" => replaceTag(el, "blockquote")
+        case "hide" => handle_hide(el)
+        case "full" | "wrapicon" | "no_icon" => el.unwrap()
         case _ =>
       }
     }
@@ -29,5 +26,21 @@ object SimpleTagConverter extends ConverterModule {
       el.html("<code>" + el.html() + "</code")
     }
     (el, List[RequiredLibrary](), List[String]())
+  }
+
+  private def replaceTag(el: Element, replacementTag: String) {
+    el.tagName(replacementTag)
+    el.removeAttr("class")
+  }
+
+  private def handle_hide(el: Element) {
+    el.select("a.re-collapse").remove() // remove "hide" button
+    val details = el.select("div.details").html() // save content
+    el.select("div.details").remove() // remove content from element
+    el.select("a.read-more").unwrap() // remove "show more" link
+    val summary = el.text() // save show/hide text
+    // replace old div-style element, with new details-summary element
+    val newEl: Element = Jsoup.parseBodyFragment(s"<details><summary>$summary</summary>$details</details>").select("details")(0)
+    el.replaceWith(newEl)
   }
 }
