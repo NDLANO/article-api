094baecb9d0f8719c68c6c4fe75f2eede4044879
Henrik Nårstad <henrik.narstad@knowit.no>
2016-06-21 14:11:21 +0200

    NDLA-305: Tester for forskjellige insertion metoder for fagstoff

--- src/test/scala/no/ndla/contentapi/service/converters/contentbrowser/FagstoffConverterTest.scala
+++ src/test/scala/no/ndla/contentapi/service/converters/contentbrowser/FagstoffConverterTest.scala
@@ -2,6 +2,7 @@ package no.ndla.contentapi.service.converters.contentbrowser
 
 import no.ndla.contentapi.{TestEnvironment, UnitSuite}
 import no.ndla.contentapi.integration.ContentFagstoff
+import no.ndla.contentapi.ContentApiProperties.ndlaBaseHost
 import org.mockito.Mockito._
 
 class FagstoffConverterTest extends UnitSuite with TestEnvironment {
@@ -30,6 +31,64 @@ class FagstoffConverterTest extends UnitSuite with TestEnvironment {
     val (result, requiredLibraries, status) = FagstoffConverter.convert(content)
 
     result should equal (s"{Import error: Failed to retrieve 'fagstoff' with language 'nb' ($nodeId)}")
+    status.nonEmpty should equal (true)
+    requiredLibraries.isEmpty should equal (true)
+  }
+
+  test("That FagstoffConverter inserts the content if insertion mode is 'collapsed_body'") {
+    val contentString = s"[contentbrowser ==nid=$nodeId==imagecache=Fullbredde==width===alt=$altText==link===node_link=1==link_type=link_to_content==lightbox_size===remove_fields[76661]=1==remove_fields[76663]=1==remove_fields[76664]=1==remove_fields[76666]=1==insertion=collapsed_body==link_title_text===link_text=Tittel==text_align===css_class=contentbrowser contentbrowser]"
+    val content = ContentBrowser(contentString, Some("nb"))
+    val expectedResult = s"<details><summary>Tittel</summary>${sampleFagstoff1.fagstoff}</details>"
+
+    when(extractService.getNodeFagstoff(nodeId)).thenReturn(Seq(sampleFagstoff1, sampleFagstoff2))
+    val (result, requiredLibraries, status) = FagstoffConverter.convert(content)
+    val strippedResult = " +".r.replaceAllIn(result.replace("\n", ""), " ")
+
+    strippedResult should equal (expectedResult)
+    status.isEmpty should equal (true)
+    requiredLibraries.isEmpty should equal (true)
+  }
+
+
+  test("That FagstoffConverter inserts the content if insertion mode is 'link'") {
+    val contentString = s"[contentbrowser ==nid=$nodeId==imagecache=Fullbredde==width===alt=$altText==link===node_link=1==link_type=link_to_content==lightbox_size===remove_fields[76661]=1==remove_fields[76663]=1==remove_fields[76664]=1==remove_fields[76666]=1==insertion=link==link_title_text===link_text=Tittel==text_align===css_class=contentbrowser contentbrowser]"
+    val content = ContentBrowser(contentString, Some("nb"))
+    val expectedResult = s"""<a href="$ndlaBaseHost/node/$nodeId">Tittel</a>"""
+
+    when(extractService.getNodeFagstoff(nodeId)).thenReturn(Seq(sampleFagstoff1, sampleFagstoff2))
+    val (result, requiredLibraries, status) = FagstoffConverter.convert(content)
+    val strippedResult = " +".r.replaceAllIn(result.replace("\n", ""), " ")
+
+    strippedResult should equal (expectedResult)
+    status.nonEmpty should equal (true)
+    requiredLibraries.isEmpty should equal (true)
+  }
+
+  test("That FagstoffConverter inserts the content if insertion mode is 'lightbox_large'") {
+    val contentString = s"[contentbrowser ==nid=$nodeId==imagecache=Fullbredde==width===alt=$altText==link===node_link=1==link_type=link_to_content==lightbox_size===remove_fields[76661]=1==remove_fields[76663]=1==remove_fields[76664]=1==remove_fields[76666]=1==insertion=lightbox_large==link_title_text===link_text=Tittel==text_align===css_class=contentbrowser contentbrowser]"
+    val content = ContentBrowser(contentString, Some("nb"))
+    val expectedResult = s"""<a href="$ndlaBaseHost/node/$nodeId">Tittel</a>"""
+
+    when(extractService.getNodeFagstoff(nodeId)).thenReturn(Seq(sampleFagstoff1, sampleFagstoff2))
+    val (result, requiredLibraries, status) = FagstoffConverter.convert(content)
+    val strippedResult = " +".r.replaceAllIn(result.replace("\n", ""), " ")
+
+    strippedResult should equal (expectedResult)
+    status.nonEmpty should equal (true)
+    requiredLibraries.isEmpty should equal (true)
+  }
+
+
+  test("That FagstoffConverter defaults to 'link' if the insertion method is unknown") {
+    val contentString = s"[contentbrowser ==nid=$nodeId==imagecache=Fullbredde==width===alt=$altText==link===node_link=1==link_type=link_to_content==lightbox_size===remove_fields[76661]=1==remove_fields[76663]=1==remove_fields[76664]=1==remove_fields[76666]=1==insertion=lightbox_large==link_title_text===link_text=Tittel==text_align===css_class=contentbrowser contentbrowser]"
+    val content = ContentBrowser(contentString, Some("nb"))
+    val expectedResult = s"""<a href="$ndlaBaseHost/node/$nodeId">Tittel</a>"""
+
+    when(extractService.getNodeFagstoff(nodeId)).thenReturn(Seq(sampleFagstoff1, sampleFagstoff2))
+    val (result, requiredLibraries, status) = FagstoffConverter.convert(content)
+    val strippedResult = " +".r.replaceAllIn(result.replace("\n", ""), " ")
+
+    strippedResult should equal (expectedResult)
     status.nonEmpty should equal (true)
     requiredLibraries.isEmpty should equal (true)
   }
