a19d8a5d88caf2dc64b5396bb0eaa039931459a0
Henrik Nårstad <henrik.narstad@knowit.no>
2016-06-22 16:19:50 +0200

    NDLA-336: Tester for NonExistentNodeConverter

--- src/test/scala/no/ndla/contentapi/service/converters/contentbrowser/NonExistentNodeConverterTest.scala
+++ src/test/scala/no/ndla/contentapi/service/converters/contentbrowser/NonExistentNodeConverterTest.scala
@@ -1,8 +1,17 @@
 package no.ndla.contentapi.service.converters.contentbrowser
 
-/**
-  * Created by henrik on 22/06/16.
-  */
-class NonExistentNodeConverterTest {
+import no.ndla.contentapi.{TestEnvironment, UnitSuite}
 
+
+class NonExistentNodeConverterTest extends UnitSuite with TestEnvironment {
+  val nodeId = "1234"
+  val contentString = s"[contentbrowser ==nid=$nodeId==imagecache=Fullbredde==width===alt=Melon==link===node_link=1==link_type=link_to_content==lightbox_size===remove_fields[76661]=1==remove_fields[76663]=1==remove_fields[76664]=1==remove_fields[76666]=1==insertion===link_title_text= ==link_text= ==text_align===css_class=contentbrowser contentbrowser]"
+
+  test("That NonExistentNodeConverter returns an empty string and a message") {
+    val content = ContentBrowser(contentString, Some("nb"))
+    val (result, requiredLibraries, messages) = NonExistentNodeConverter.convert(content)
+
+    result should equal ("")
+    messages.nonEmpty should be (true)
+  }
 }
