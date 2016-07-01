c45148f8bee1b9b42b44e44faf7a07ae0c444f92
Henrik Nårstad <henrik.narstad@knowit.no>
2016-07-01 13:28:19 +0200

    NDLA-345: Test GeneralContentConverter

--- src/main/scala/no/ndla/contentapi/service/converters/contentbrowser/ContentBrowserConverterModule.scala
+++ src/main/scala/no/ndla/contentapi/service/converters/contentbrowser/ContentBrowserConverterModule.scala
@@ -11,5 +11,20 @@ trait ContentBrowserConverterModule {
   val typeName: String
 }
 
-trait ContentBrowserConverterModules extends ExtractServiceComponent with CMDataComponent with StorageService with AmazonClientComponent with ImageApiServiceComponent with ImageConverterModule with LenkeConverterModule with H5PConverterModule with OppgaveConverterModule with FagstoffConverterModule with NonExistentNodeConverterModule with AudioConverterModule with AktualitetConverterModule with VideoConverterModule with VeiledningConverterModule
+trait ContentBrowserConverterModules
+  extends ExtractServiceComponent
+  with CMDataComponent
+  with StorageService
+  with AmazonClientComponent
+  with ImageApiServiceComponent
+  with ImageConverterModule
+  with LenkeConverterModule
+  with H5PConverterModule
+  with OppgaveConverterModule
+  with FagstoffConverterModule
+  with NonExistentNodeConverterModule
+  with AudioConverterModule
+  with AktualitetConverterModule
+  with VideoConverterModule
+  with VeiledningConverterModule
 
--- src/test/scala/no/ndla/contentapi/service/converters/contentbrowser/GeneralContentConverterTest.scala
+++ src/test/scala/no/ndla/contentapi/service/converters/contentbrowser/GeneralContentConverterTest.scala
@@ -5,7 +5,7 @@ import no.ndla.contentapi.integration.NodeGeneralContent
 import no.ndla.contentapi.ContentApiProperties.ndlaBaseHost
 import org.mockito.Mockito._
 
-class FagstoffConverterTest extends UnitSuite with TestEnvironment {
+class GeneralContentConverterTest extends UnitSuite with TestEnvironment {
   val (nodeId, nodeId2) = ("1234", "4321")
   val insertion = "inline"
   val altText = "Jente som spiser melom. Grønn bakgrunn, rød melon. Fotografi."
@@ -13,29 +13,33 @@ class FagstoffConverterTest extends UnitSuite with TestEnvironment {
   val sampleFagstoff1 = NodeGeneralContent(nodeId, nodeId, "Tittel", "Innhold", "nb")
   val sampleFagstoff2 = NodeGeneralContent(nodeId, nodeId2, "Tittel", "Innhald", "nn")
 
-  test("That FagstoffConverter returns the contents of a fagstoff according to language") {
+  val generalContentConverter = new GeneralContentConverter {
+    override val typeName: String = "test"
+  }
+
+  test("That GeneralContentConverter returns the contents according to language") {
     val content = ContentBrowser(contentString, Some("nb"))
 
     when(extractService.getNodeGeneralContent(nodeId)).thenReturn(Seq(sampleFagstoff1, sampleFagstoff2))
-    val (result, requiredLibraries, status) = FagstoffConverter.convert(content)
+    val (result, requiredLibraries, status) = generalContentConverter.convert(content)
 
     result should equal (sampleFagstoff1.content)
     status.isEmpty should equal (true)
     requiredLibraries.isEmpty should equal (true)
   }
 
-  test("That FagstoffConverter returns an error when the fagstoff is not found") {
+  test("That GeneralContentConverter returns an error when the node is not found") {
     val content = ContentBrowser(contentString, Some("nb"))
 
     when(extractService.getNodeGeneralContent(nodeId)).thenReturn(Seq())
-    val (result, requiredLibraries, status) = FagstoffConverter.convert(content)
+    val (result, requiredLibraries, status) = generalContentConverter.convert(content)
 
-    result should equal (s"{Import error: Failed to retrieve 'fagstoff' with language 'nb' ($nodeId)}")
+    result should equal (s"{Import error: Failed to retrieve '${generalContentConverter.typeName}' with language 'nb' ($nodeId)}")
     status.nonEmpty should equal (true)
     requiredLibraries.isEmpty should equal (true)
   }
 
-  test("That FagstoffConverter inserts the content if insertion mode is 'collapsed_body'") {
+  test("That GeneralContentConverter inserts the content if insertion mode is 'collapsed_body'") {
     val contentString = s"[contentbrowser ==nid=$nodeId==imagecache=Fullbredde==width===alt=$altText==link===node_link=1==link_type=link_to_content==lightbox_size===remove_fields[76661]=1==remove_fields[76663]=1==remove_fields[76664]=1==remove_fields[76666]=1==insertion=collapsed_body==link_title_text===link_text=Tittel==text_align===css_class=contentbrowser contentbrowser]"
     val content = ContentBrowser(contentString, Some("nb"))
     val expectedResult = s"<details><summary>Tittel</summary>${sampleFagstoff1.content}</details>"
@@ -50,7 +54,7 @@ class FagstoffConverterTest extends UnitSuite with TestEnvironment {
   }
 
 
-  test("That FagstoffConverter inserts the content if insertion mode is 'link'") {
+  test("That GeneralContentConverter inserts the content if insertion mode is 'link'") {
     val contentString = s"[contentbrowser ==nid=$nodeId==imagecache=Fullbredde==width===alt=$altText==link===node_link=1==link_type=link_to_content==lightbox_size===remove_fields[76661]=1==remove_fields[76663]=1==remove_fields[76664]=1==remove_fields[76666]=1==insertion=link==link_title_text===link_text=Tittel==text_align===css_class=contentbrowser contentbrowser]"
     val content = ContentBrowser(contentString, Some("nb"))
     val expectedResult = s"""<a href="$ndlaBaseHost/node/$nodeId">Tittel</a>"""
@@ -64,7 +68,7 @@ class FagstoffConverterTest extends UnitSuite with TestEnvironment {
     requiredLibraries.isEmpty should equal (true)
   }
 
-  test("That FagstoffConverter inserts the content if insertion mode is 'lightbox_large'") {
+  test("That GeneralContentConverter inserts the content if insertion mode is 'lightbox_large'") {
     val contentString = s"[contentbrowser ==nid=$nodeId==imagecache=Fullbredde==width===alt=$altText==link===node_link=1==link_type=link_to_content==lightbox_size===remove_fields[76661]=1==remove_fields[76663]=1==remove_fields[76664]=1==remove_fields[76666]=1==insertion=lightbox_large==link_title_text===link_text=Tittel==text_align===css_class=contentbrowser contentbrowser]"
     val content = ContentBrowser(contentString, Some("nb"))
     val expectedResult = s"""<a href="$ndlaBaseHost/node/$nodeId">Tittel</a>"""
@@ -78,8 +82,7 @@ class FagstoffConverterTest extends UnitSuite with TestEnvironment {
     requiredLibraries.isEmpty should equal (true)
   }
 
-
-  test("That FagstoffConverter defaults to 'link' if the insertion method is unknown") {
+  test("That GeneralContentConverter defaults to 'link' if the insertion method is unknown") {
     val contentString = s"[contentbrowser ==nid=$nodeId==imagecache=Fullbredde==width===alt=$altText==link===node_link=1==link_type=link_to_content==lightbox_size===remove_fields[76661]=1==remove_fields[76663]=1==remove_fields[76664]=1==remove_fields[76666]=1==insertion=lightbox_large==link_title_text===link_text=Tittel==text_align===css_class=contentbrowser contentbrowser]"
     val content = ContentBrowser(contentString, Some("nb"))
     val expectedResult = s"""<a href="$ndlaBaseHost/node/$nodeId">Tittel</a>"""
