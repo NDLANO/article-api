311a6adf3f95508def55dc6bbe2e56592be6513c
Henrik Nårstad <henrik.narstad@knowit.no>
2016-10-06 13:22:41 +0200

    #22 alignment for images

--- src/main/scala/no/ndla/articleapi/service/converters/contentbrowser/ImageConverterModule.scala
+++ src/main/scala/no/ndla/articleapi/service/converters/contentbrowser/ImageConverterModule.scala
@@ -18,7 +18,7 @@ import no.ndla.articleapi.service.converters.HtmlFigureGenerator
 trait ImageConverterModule {
   this: ImageApiServiceComponent =>
 
-  object ImageConverter extends ContentBrowserConverterModule with LazyLogging{
+  object ImageConverter extends ContentBrowserConverterModule with LazyLogging {
     override val typeName: String = "image"
 
     override def convert(content: ContentBrowser, visitedNodes: Seq[String]): (String, Seq[RequiredLibrary], ImportStatus) = {
@@ -28,12 +28,14 @@ trait ImageConverterModule {
     }
 
     def getImage(cont: ContentBrowser): (String, Seq[String]) = {
+      val alignment = getImageAlignment(cont)
       val figureDataAttributes = Map(
         "resource" -> "image",
         "size" -> cont.get("imagecache").toLowerCase,
         "alt" -> cont.get("alt"),
         "caption" -> cont.get("link_text"),
-        "id" -> s"${cont.id}"
+        "id" -> s"${cont.id}",
+        "align" -> alignment.getOrElse("")
       )
 
       imageApiService.importOrGetMetaByExternId(cont.get("nid")) match {
@@ -42,6 +44,18 @@ trait ImageConverterModule {
         case None =>
           (s"<img src='stock.jpeg' alt='The image with id ${cont.get("nid")} was not not found' />",
             Seq(s"Image with id ${cont.get("nid")} was not found"))
+      }
+    }
+
+    private def getImageAlignment(cont: ContentBrowser): Option[String] = {
+      val marginCssClass = cont.get("css_class").split(" ").find(_.contains("margin"))
+      val margin = marginCssClass.flatMap(margin =>"""contentbrowser_margin_(left|right)$""".r.findFirstMatchIn(margin).map(_.group(1)))
+
+      // right margin = left alignment, left margin = right alignment
+      margin match {
+        case Some("right") => Some("left")
+        case Some("left") => Some("right")
+        case _ => None
       }
     }
 
--- src/test/scala/no/ndla/articleapi/service/converters/contentbrowser/ContentBrowserConverterTest.scala
+++ src/test/scala/no/ndla/articleapi/service/converters/contentbrowser/ContentBrowserConverterTest.scala
@@ -52,7 +52,7 @@ class ContentBrowserConverterTest extends UnitSuite with TestEnvironment {
     val imageMeta = ImageMetaInformation(newId, List(), List(), ImageVariants(Some(Image("small.jpeg", 128, "")), Some(Image(imageUrl, 256, ""))), Copyright(License("", "", Some("")), "", List()), List())
     val expectedResult =
       s"""|<article>
-          |<figure data-size="fullbredde" data-url="http://localhost/images/$newId" data-id="1" data-resource="image" data-alt="$alt" data-caption=""></figure>
+          |<figure data-size="fullbredde" data-url="http://localhost/images/$newId" data-align="" data-id="1" data-resource="image" data-alt="$alt" data-caption=""></figure>
           |</article>""".stripMargin.replace("\n", "")
 
     when(extractService.getNodeType(nodeId)).thenReturn(Some("image"))
