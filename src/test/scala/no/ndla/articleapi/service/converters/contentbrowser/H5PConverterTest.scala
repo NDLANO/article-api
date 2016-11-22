bbe994eb9c2d613d411e808313857ae1ebe46c41
Henrik Nårstad <henrik.narstad@knowit.no>
2016-11-22 15:46:34 +0100

    add tests for H5P converter #110

--- src/test/scala/no/ndla/articleapi/service/converters/contentbrowser/H5PConverterTest.scala
+++ src/test/scala/no/ndla/articleapi/service/converters/contentbrowser/H5PConverterTest.scala
@@ -13,29 +13,17 @@ import no.ndla.articleapi.{TestEnvironment, UnitSuite}
 import no.ndla.articleapi.ArticleApiProperties.resourceHtmlEmbedTag
 
 class H5PConverterTest extends UnitSuite with TestEnvironment {
-  val (validNodeId, joubelH5PIdId) = ValidH5PNodeIds.head
-  val invalidNodeId = "123123"
+  val nodeId = "1234"
   val altText = "Jente som spiser melom. Grønn bakgrunn, rød melon. Fotografi."
-  val contentStringWithValidNodeId = s"[contentbrowser ==nid=$validNodeId==imagecache=Fullbredde==width===alt=$altText==link===node_link=1==link_type=link_to_content==lightbox_size===remove_fields[76661]=1==remove_fields[76663]=1==remove_fields[76664]=1==remove_fields[76666]=1==insertion===link_title_text= ==link_text= ==text_align===css_class=contentbrowser contentbrowser]"
-  val contentStringWithInvalidNodeId = s"[contentbrowser ==nid=$invalidNodeId==imagecache=Fullbredde==width===alt=$altText==link===node_link=1==link_type=link_to_content==lightbox_size===remove_fields[76661]=1==remove_fields[76663]=1==remove_fields[76664]=1==remove_fields[76666]=1==insertion===link_title_text= ==link_text= ==text_align===css_class=contentbrowser contentbrowser]"
+  val contentString = s"[contentbrowser ==nid=$nodeId==imagecache=Fullbredde==width===alt=$altText==link===node_link=1==link_type=link_to_content==lightbox_size===remove_fields[76661]=1==remove_fields[76663]=1==remove_fields[76664]=1==remove_fields[76666]=1==insertion===link_title_text= ==link_text= ==text_align===css_class=contentbrowser contentbrowser]"
+  val content = ContentBrowser(contentString, Some("nb"), 1)
 
-  test("Contentbrowser strings of type 'h5p_content' returns a h5p embed-resource") {
-    val expectedResult = s"""<$resourceHtmlEmbedTag data-id="1" data-resource="h5p" data-url="${JoubelH5PConverter.JoubelH5PBaseUrl}/$joubelH5PIdId" />"""
-    val content = ContentBrowser(contentStringWithValidNodeId, Some("nb"), 1)
-    val (result, requiredLibraries, errors) = JoubelH5PConverter.convert(content, Seq())
+  test("That contentbrowser strings of type 'h5p_content' returns an iframe") {
+    val expectedResult = s"""<$resourceHtmlEmbedTag data-id="1" data-resource="h5p" data-url="http://ndla.no/h5p/embed/1234" />"""
+    val (result, requiredLibraries, errors) = H5PConverter.convert(content, Seq())
 
     result should equal(expectedResult)
     errors.messages.length should equal(0)
-    requiredLibraries.length should equal (0)
-  }
-
-  test("H5P nodes with an invalid node id returns an error embed-resource") {
-    val expectedResult = s"""<$resourceHtmlEmbedTag data-id="1" data-message="H5P node $invalidNodeId is not yet exported to new H5P service" data-resource="error" />"""
-    val content = ContentBrowser(contentStringWithInvalidNodeId, Some("nb"), 1)
-    val (result, requiredLibraries, errors) = JoubelH5PConverter.convert(content, Seq())
-
-    result should equal(expectedResult)
-    errors.messages.length should equal(1)
-    requiredLibraries.length should equal (0)
+    requiredLibraries.length should be > 0
   }
 }
