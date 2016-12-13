46ed3241b99f81903838210e3a1173e123db620c
Henrik Nårstad <henrik.narstad@knowit.no>
2016-12-13 15:51:48 +0100

    if present, move ingress-image to content #129

--- src/main/scala/no/ndla/articleapi/service/converters/HTMLCleaner.scala
+++ src/main/scala/no/ndla/articleapi/service/converters/HTMLCleaner.scala
@@ -2,121 +2,144 @@ package no.ndla.articleapi.service.converters
 
 import com.typesafe.scalalogging.LazyLogging
 import no.ndla.articleapi.ArticleApiProperties._
-import no.ndla.articleapi.integration.{ConverterModule, LanguageContent, LanguageIngress}
+import no.ndla.articleapi.integration.{ConverterModule, ImageApiClient, LanguageContent, LanguageIngress}
 import no.ndla.articleapi.model.domain.ImportStatus
-import org.jsoup.nodes.{TextNode, Element, Node}
+import org.jsoup.nodes.{Element, Node, TextNode}
 
 import scala.collection.JavaConversions._
 
-object HTMLCleaner extends ConverterModule with LazyLogging {
-  override def convert(content: LanguageContent, importStatus: ImportStatus): (LanguageContent, ImportStatus) = {
-    val element = stringToJsoupDocument(content.content)
+trait HTMLCleaner {
+  this: ImageApiClient with HtmlTagGenerator =>
+  val htmlCleaner: HTMLCleaner
 
-    val illegalTags = unwrapIllegalTags(element).map(x => s"Illegal tag(s) removed: $x").distinct
-    val illegalAttributes = removeAttributes(element).map(x => s"Illegal attribute(s) removed: $x").distinct
+  class HTMLCleaner extends ConverterModule with LazyLogging {
 
-    removeComments(element)
-    removeNbsp(element)
-    removeEmptyTags(element)
+    override def convert(content: LanguageContent, importStatus: ImportStatus): (LanguageContent, ImportStatus) = {
+      val element = stringToJsoupDocument(content.content)
+      val illegalTags = unwrapIllegalTags(element).map(x => s"Illegal tag(s) removed: $x").distinct
+      val illegalAttributes = removeAttributes(element).map(x => s"Illegal attribute(s) removed: $x").distinct
 
-    wrapStandaloneTextInPTag(element)
+      removeComments(element)
+      removeNbsp(element)
+      removeEmptyTags(element)
+      wrapStandaloneTextInPTag(element)
 
-    val ingressLanguage = content.ingress match {
-      case Some(ingress) => Some(LanguageIngress(extractIngressText(stringToJsoupDocument(ingress.content))))
-      case None => extractIngress(element)
+      val ingress = getIngress(content, element)
+
+      (content.copy(content=jsoupDocumentToString(element), ingress=ingress),
+        ImportStatus(importStatus.messages ++ illegalTags ++ illegalAttributes, importStatus.visitedNodes))
     }
 
-    (content.copy(content=jsoupDocumentToString(element), ingress=ingressLanguage),
-      ImportStatus(importStatus.messages ++ illegalTags ++ illegalAttributes, importStatus.visitedNodes))
-  }
 
-  private def unwrapIllegalTags(el: Element): Seq[String] = {
-    el.select("*").toList.
-      filter(x => !isTagValid(x.tagName))
-      .map(x => {
-        val tagName = x.tagName
-        x.unwrap()
-        tagName
+    private def getIngress(content: LanguageContent, element: Element): Option[LanguageIngress] = {
+      content.ingress match {
+        case None => extractIngress(element)
+        case Some(ingress) =>
+          val (imageEmbedHtml) = ingress.ingressImage.flatMap(imageApiClient.importOrGetMetaByExternId)
+            .map(imageMetaData => HtmlTagGenerator.buildImageEmbedContent(
+              caption="",
+              imageId=imageMetaData.id.toString,
+              align="",
+              size="",
+              altText=imageMetaData.alttexts.find(_.language==content.language).map(_.alttext).getOrElse("")))
+
+          imageEmbedHtml.map(x => element.prepend(x._1))
+
+          Some(LanguageIngress(extractIngressText(stringToJsoupDocument(ingress.content)), None))
+      }
+    }
+
+    private def unwrapIllegalTags(el: Element): Seq[String] = {
+      el.select("*").toList.
+        filter(x => !HTMLCleaner.isTagValid(x.tagName))
+        .map(x => {
+          val tagName = x.tagName
+          x.unwrap()
+          tagName
+        })
+        .distinct
+    }
+
+    private def removeAttributes(el: Element): Seq[String] = {
+      el.select("*").toList.flatMap(tag => {
+        tag.attributes().toList.
+          filter(attribute => !HTMLCleaner.isAttributeKeyValid(attribute.getKey, tag.tagName))
+          .map(illegalAttribute => {
+            val keyName = illegalAttribute.getKey
+            tag.removeAttr(keyName)
+            keyName
+        })
       })
-      .distinct
-  }
+    }
 
-  private def removeAttributes(el: Element): Seq[String] = {
-    el.select("*").toList.flatMap(tag => {
-      tag.attributes().toList.
-        filter(attribute => !isAttributeKeyValid(attribute.getKey, tag.tagName))
-        .map(illegalAttribute => {
-          val keyName = illegalAttribute.getKey
-          tag.removeAttr(keyName)
-          keyName
-      })
-    })
-  }
+    private def removeComments(node: Node) {
+      var i = 0
 
-  private def removeComments(node: Node) {
-    var i = 0
+      while (i < node.childNodes().size()) {
+        val child = node.childNode(i)
 
-    while (i < node.childNodes().size()) {
-      val child = node.childNode(i)
-
-      child.nodeName() == "#comment" match {
-        case true => child.remove()
-        case false => {
-          i+= 1
-          removeComments(child)
+        child.nodeName() == "#comment" match {
+          case true => child.remove()
+          case false => {
+            i+= 1
+            removeComments(child)
+          }
         }
       }
     }
-  }
 
-  private def removeEmptyTags(element: Element): Element = {
-    for (el <- element.select("p,div,section")) {
-      if (el.select(resourceHtmlEmbedTag).isEmpty && !el.hasText && el.isBlock) {
-        el.remove()
+    private def removeEmptyTags(element: Element): Element = {
+      for (el <- element.select("p,div,section")) {
+        if (el.select(resourceHtmlEmbedTag).isEmpty && !el.hasText && el.isBlock) {
+          el.remove()
+        }
+      }
+
+      element
+    }
+
+    private def removeNbsp(el: Element) {
+      el.html(el.html().replace("\u00a0", "")) // \u00a0 is the unicode representation of &nbsp;
+    }
+
+    private def getIngressText(el: Element): Option[Element] = {
+      val firstSection = Option(el.select("body>section").first)
+      val ingress = firstSection.flatMap(section => Option(section.select(">p>strong").first))
+
+      ingress match {
+        case None => firstSection.flatMap(section => Option(section.select(">strong").first))
+        case x => x
       }
     }
 
-    element
-  }
+    private def extractIngress(el: Element): (Option[LanguageIngress]) = {
+      val ingressText = getIngressText(el).map(ingress => extractIngressText(ingress))
 
-  private def removeNbsp(el: Element) {
-    el.html(el.html().replace("\u00a0", "")) // \u00a0 is the unicode representation of &nbsp;
-  }
+      removeEmptyTags(el)
+      ingressText.map(text => LanguageIngress(text, None))
+    }
 
-  private def getIngressText(el: Element): Option[Element] = {
-    val firstSection = Option(el.select("body>section").first)
-    val ingress = firstSection.flatMap(section => Option(section.select(">p>strong").first))
+    private def extractIngressText(ingressTextElement: Element): String = {
+      ingressTextElement.remove()
+      ingressTextElement.text()
+    }
 
-    ingress match {
-      case None => firstSection.flatMap(section => Option(section.select(">strong").first))
-      case x => x
+    private def wrapStandaloneTextInPTag (element: Element) : Element = {
+      val sections = element.select("body>section")
+      sections.map(node => node.childNodes().map(child => {
+        if (child.nodeName() == "#text" && !child.asInstanceOf[TextNode].isBlank) {
+          child.wrap("<p>")
+        }
+        child
+      }))
+
+      element
     }
   }
 
-  private def extractIngress(el: Element): (Option[LanguageIngress]) = {
-    val ingressText = getIngressText(el).map(ingress => extractIngressText(ingress))
+}
 
-    removeEmptyTags(el)
-    ingressText.map(text => LanguageIngress(text))
-  }
-
-  private def extractIngressText(ingressTextElement: Element): String = {
-    ingressTextElement.remove()
-    ingressTextElement.text()
-  }
-
-  private def wrapStandaloneTextInPTag (element: Element) : Element = {
-    val sections = element.select("body>section")
-    sections.map(node => node.childNodes().map(child => {
-      if (child.nodeName() == "#text" && !child.asInstanceOf[TextNode].isBlank) {
-        child.wrap("<p>")
-      }
-      child
-    }))
-
-    element
-  }
-
+object HTMLCleaner {
   private object PermittedHTML {
     // MathML element reference list: https://developer.mozilla.org/en/docs/Web/MathML/Element
     private val mathJaxTags = Set("math", "maction", "maligngroup", "malignmark", "menclose", "merror", "mfenced", "mfrac", "mglyph", "mi",
