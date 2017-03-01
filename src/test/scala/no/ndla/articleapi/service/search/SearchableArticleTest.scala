41284242e42652a4b2eb7247af132d0ee4a75e9f
Tanja Gruschke <tmg@knowit.no>
2017-03-01 11:42:12 +0100

    #220 test

--- src/test/scala/no/ndla/articleapi/service/search/SearchableArticleTest.scala
+++ src/test/scala/no/ndla/articleapi/service/search/SearchableArticleTest.scala
@@ -15,6 +15,8 @@ class SearchableArticleTest extends UnitSuite with TestEnvironment {
   test("Language with empty Some should convert language to None") {
     val lv = LanguageValue(Some(""), "This is ikke bare una Sprache")
     lv.lang should equal(None)
+    lv.value should equal("This is ikke bare una Sprache")
+    lv should equal(LanguageValue(None, "This is ikke bare una Sprache"))
   }
 
 }
