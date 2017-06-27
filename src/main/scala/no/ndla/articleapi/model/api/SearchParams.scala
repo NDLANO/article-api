cc539fad46bd760321262f0a3a801f9a63f2e00d
narhen <henrik.narstad@knowit.no>
2017-06-27 15:57:35 +0200

    #453: update swagger doc

--- src/main/scala/no/ndla/articleapi/model/api/SearchParams.scala
+++ src/main/scala/no/ndla/articleapi/model/api/SearchParams.scala
@@ -1,10 +1,9 @@
 package no.ndla.articleapi.model.api
 
-import no.ndla.articleapi.model.domain.Sort
 import org.scalatra.swagger.annotations.{ApiModel, ApiModelProperty}
 import scala.annotation.meta.field
 
-@ApiModel(description = "Information about the article")
+@ApiModel(description = "The search parameters")
 case class SearchParams(@(ApiModelProperty@field)(description = "The search query") query: Option[String],
                         @(ApiModelProperty@field)(description = "The ISO 639-1 language code describing language used in query-params") language: Option[String],
                         @(ApiModelProperty@field)(description = "Return only articles with provided license.") license: Option[String],
