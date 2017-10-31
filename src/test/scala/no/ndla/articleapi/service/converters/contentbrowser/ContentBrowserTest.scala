53b503860ef6ac0cebc7691748edfc1d82841b56
Henrik Nårstad <henrik.narstad@knowit.no>
2017-10-31 12:53:22 +0100

    fix unhandled contentbrowser string #725

--- src/test/scala/no/ndla/articleapi/service/converters/contentbrowser/ContentBrowserTest.scala
+++ src/test/scala/no/ndla/articleapi/service/converters/contentbrowser/ContentBrowserTest.scala
@@ -15,26 +15,29 @@ class ContentBrowserTest extends UnitSuite with TestEnvironment {
   val nodeId = "1234"
   val altText = "Jente som spiser melom. Grønn bakgrunn, rød melon. Fotografi."
   val contentString = s"[contentbrowser ==nid=$nodeId==imagecache=Fullbredde==width===alt=$altText==link===node_link=1==link_type=link_to_content==lightbox_size===remove_fields[76661]=1==remove_fields[76663]=1==remove_fields[76664]=1==remove_fields[76666]=1==insertion===link_title_text= ==link_text= ==text_align===css_class=contentbrowser contentbrowser]"
-  val altContentBrowser = s"[contentbrowser ==nid=$nodeId==imagecache=Hoyrespalte==width=180==alt=Enkle munnbind. Foto.==link===node_link=1==link_type=link_to_content==lightbox_size===remove_fields[76661]=1==remove_fields[76663]=1==remove_fields[76664]=1==remove_fields[76666]=1==insertion===link_title_text=Enkle munnbind.==link_text=Enkle munnbind.==text_align=right==css_class===css_class=contentbrowser contentbrowser_margin_left]"
+  val LeftMarginContentBrowser = s"[contentbrowser ==nid=$nodeId==imagecache=Hoyrespalte==width=180==alt=Enkle munnbind. Foto.==link===node_link=1==link_type=link_to_content==lightbox_size===remove_fields[76661]=1==remove_fields[76663]=1==remove_fields[76664]=1==remove_fields[76666]=1==insertion===link_title_text=Enkle munnbind.==link_text=Enkle munnbind.==text_align=right==css_class===css_class=contentbrowser contentbrowser_margin_left]"
+  val RightContentBrowser = "[contentbrowser ==nid=24683==imagecache=Hoyrespalte==width===alt=tikroning, mynt. Foto.==link===node_link=1==link_type=link_to_content==lightbox_size===remove_fields[76661]=1==remove_fields[76666]=1==insertion===link_title_text= ==link_text= ==text_align=left==css_class===css_class=contentbrowser contentbrowser_margin_right]"
   val validContentBrowser = ContentBrowser(contentString, "nb")
-  val altValidContentBrowser = ContentBrowser(altContentBrowser, "nb")
+  val altValidContentBrowser = ContentBrowser(LeftMarginContentBrowser, "nb")
+  val alt2ValidContentBrowser = ContentBrowser(RightContentBrowser, "nb")
   val invalidContentBrowser = ContentBrowser(contentString.substring(1), "nb")
 
   test("That isContentBrowserField returns true for a valid contentbrowser string") {
-    validContentBrowser.isContentBrowserField() should equal(true)
-    altValidContentBrowser.isContentBrowserField() should equal(true)
+    validContentBrowser.IsContentBrowserField should equal(true)
+    altValidContentBrowser.IsContentBrowserField should equal(true)
+    alt2ValidContentBrowser.IsContentBrowserField should equal(true)
   }
 
   test("That isContentBrowserField returns true for an invalid contentbrowser string") {
-    invalidContentBrowser.isContentBrowserField() should equal(false)
+    invalidContentBrowser.IsContentBrowserField should equal(false)
   }
 
   test("That getStartEndIndex returns the correct start and end indexes") {
-    validContentBrowser.getStartEndIndex() should equal((0, 382))
-    ContentBrowser("Innhold! " + contentString + "also junk", "nb").getStartEndIndex() should equal((9, 391))
+    validContentBrowser.StartEndIndex should equal((0, 382))
+    ContentBrowser("Innhold! " + contentString + "also junk", "nb").StartEndIndex should equal((9, 391))
 
-    altValidContentBrowser.getStartEndIndex() should equal((0, 403))
-    ContentBrowser("junk junk" + altContentBrowser + "also junk", "nb").getStartEndIndex() should equal((9, 412))
+    altValidContentBrowser.StartEndIndex should equal((0, 403))
+    ContentBrowser("junk junk" + LeftMarginContentBrowser + "also junk", "nb").StartEndIndex should equal((9, 412))
   }
 
   test("That get returns the associated value if the key exists") {
