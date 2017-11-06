179f64a094ed134d9d84b21304bd811c286da20a
Jonas Natten <jonas.natten@knowit.no>
2017-11-06 13:39:37 +0100

    #747: Fixes authortype list

--- src/main/scala/no/ndla/articleapi/ArticleApiProperties.scala
+++ src/main/scala/no/ndla/articleapi/ArticleApiProperties.scala
@@ -66,8 +66,8 @@ object ArticleApiProperties extends LazyLogging {
   val supportedContentTypes = Set("fagstoff", "oppgave", "veiledning", "aktualitet", "emneartikkel", nodeTypeBegrep, nodeTypeVideo, nodeTypeH5P)
 
   val creatorTypes = List("opphavsmann", "fotograf", "kunstner", "redaksjonelt", "forfatter", "manusforfatter", "innleser", "oversetter", "regissør", "illustratør", "medforfatter", "komponist")
-  val processorTypes = List("bearbeider", "forlag", "distributør", "leverandør")
-  val rightsholderTypes = List("rettighetshaver", "tilrettelegger", "redaksjonelt", "språklig", "ide", "sammenstiller", "korrektur")
+  val processorTypes= List("bearbeider", "tilrettelegger", "redaksjonelt", "språklig", "ide", "sammenstiller", "korrektur")
+  val rightsholderTypes =  List("rettighetshaver", "forlag", "distributør", "leverandør")
 
 
   // When converting a content node, the converter may run several times over the content to make sure
