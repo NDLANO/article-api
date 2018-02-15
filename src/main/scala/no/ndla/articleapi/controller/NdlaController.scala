75ed19fb72f1bb30bc202877b3996882f744a886
Jonas Natten <jonas.natten@knowit.no>
2018-02-15 10:00:05 +0100

    #827: Handle postgres exceptions with attempting to reconnect to database

--- src/main/scala/no/ndla/articleapi/controller/NdlaController.scala
+++ src/main/scala/no/ndla/articleapi/controller/NdlaController.scala
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletRequest
 
 import com.typesafe.scalalogging.LazyLogging
 import no.ndla.articleapi.ArticleApiProperties.{CorrelationIdHeader, CorrelationIdKey}
+import no.ndla.articleapi.ComponentRegistry
 import no.ndla.articleapi.model.api.{AccessDeniedException, Error, ImportException, ImportExceptions, NotFoundException, OptimisticLockException, ResultWindowTooLargeException, ValidationError}
 import no.ndla.articleapi.model.domain.emptySomeToNone
 import no.ndla.network.{ApplicationUrl, AuthUser, CorrelationID}
@@ -21,8 +22,10 @@ import org.apache.logging.log4j.ThreadContext
 import org.elasticsearch.index.IndexNotFoundException
 import org.json4s.native.Serialization.read
 import org.json4s.{DefaultFormats, Formats}
+import org.postgresql.util.PSQLException
 import org.scalatra.json.NativeJsonSupport
 import org.scalatra.{BadRequest, InternalServerError, NotFound, ScalatraServlet, _}
+import scalikejdbc.{ConnectionPool, DataSourceConnectionPool}
 
 import scala.util.{Failure, Success, Try}
 
@@ -53,6 +56,9 @@ abstract class NdlaController extends ScalatraServlet with NativeJsonSupport wit
     case NotFoundException(message, supportedLanguages) => NotFound(body=Error(Error.NOT_FOUND, message, supportedLanguages = Some(supportedLanguages)))
     case o: OptimisticLockException => Conflict(body=Error(Error.RESOURCE_OUTDATED, o.getMessage))
     case rw: ResultWindowTooLargeException => UnprocessableEntity(body=Error(Error.WINDOW_TOO_LARGE, rw.getMessage))
+    case _: PSQLException =>
+      ConnectionPool.singleton(new DataSourceConnectionPool(ComponentRegistry.dataSource))
+      InternalServerError(Error(Error.DATABASE_UNAVAILABLE, Error.DATABASE_UNAVAILABLE_DESCRIPTION))
     case t: Throwable =>
       logger.error(Error.GenericError.toString, t)
       InternalServerError(body=Error.GenericError)
