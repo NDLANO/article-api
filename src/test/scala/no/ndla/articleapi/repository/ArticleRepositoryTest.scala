3e02e0ac7225b3853d9f92e3cd885e815c28d14b
Jonas Natten <jonas.natten@knowit.no>
2018-10-03 13:56:16 +0200

    Adds simple test for recreating article if not in database

--- src/test/scala/no/ndla/articleapi/repository/ArticleRepositoryTest.scala
+++ src/test/scala/no/ndla/articleapi/repository/ArticleRepositoryTest.scala
@@ -9,6 +9,7 @@
 package no.ndla.articleapi.repository
 
 import java.net.Socket
+
 import no.ndla.articleapi.model.api.NotFoundException
 import no.ndla.articleapi.model.domain
 import no.ndla.articleapi.model.domain.ArticleIds
@@ -22,7 +23,7 @@ class ArticleRepositoryTest extends IntegrationSuite with TestEnvironment {
 
   lazy val sampleArticle = TestData.sampleArticleWithByNcSa
 
-  def serverIsListenning: Boolean = {
+  def serverIsListening: Boolean = {
     Try(new Socket(ArticleApiProperties.MetaServer, ArticleApiProperties.MetaPort)) match {
       case Success(c) =>
         c.close()
@@ -35,7 +36,7 @@ class ArticleRepositoryTest extends IntegrationSuite with TestEnvironment {
 
   override def beforeAll(): Unit = {
     ConnectionPool.singleton(new DataSourceConnectionPool(getDataSource))
-    if (serverIsListenning) {
+    if (serverIsListening) {
       DBMigrator.migrate(ConnectionPool.dataSource())
     }
   }
@@ -113,6 +114,16 @@ class ArticleRepositoryTest extends IntegrationSuite with TestEnvironment {
 
     repository.delete(idWithExternals)
     repository.delete(idWithoutExternals)
+  }
+
+  test("updating with a valid article with a that is not in database will be recreated") {
+    assume(databaseIsAvailable, "Database is unavailable")
+    val article = TestData.sampleDomainArticle.copy(id = Some(110))
+
+    val x = repository.updateArticleFromDraftApi(article, List.empty)
+    x.isSuccess should be(true)
+
+    repository.delete(x.get.id.get)
   }
 
 }
