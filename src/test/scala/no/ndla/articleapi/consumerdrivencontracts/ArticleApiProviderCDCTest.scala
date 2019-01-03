eea0a07f032b1cd0173a079b7cae2cc124283dbe
Jonas Natten <jonas.natten@knowit.no>
2019-01-03 14:37:57 +0100

    Fix knowing when TRAVIS build is PR

--- src/test/scala/no/ndla/articleapi/consumerdrivencontracts/ArticleApiProviderCDCTest.scala
+++ src/test/scala/no/ndla/articleapi/consumerdrivencontracts/ArticleApiProviderCDCTest.scala
@@ -107,7 +107,7 @@ class ArticleApiProviderCDCTest extends IntegrationSuite with TestEnvironment {
 
   test("That pacts from broker are working.", PactProviderTest) {
     val isTravis = envOrElse("TRAVIS", "false").toBoolean
-    val isPullRequest = envOrElse("TRAVIS_PULL_REQUEST", "false") == "false"
+    val isPullRequest = envOrElse("TRAVIS_PULL_REQUEST", "false") != "false"
     val publishResults = if (isTravis && !isPullRequest) {
       getGitVersion.map(version => BrokerPublishData(version, None)).toOption
     } else { None }
