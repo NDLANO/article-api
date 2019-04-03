8dde25a4e4add9f8274e8c36053abf30e07d8cf2
Gunnar Velle <gunnar.velle@gmail.com>
2019-04-03 13:50:04 +0200

    forgot to add code

--- src/main/scala/db/migration/V18__RemoveDummyMetaDescription.scala
+++ src/main/scala/db/migration/V18__RemoveDummyMetaDescription.scala
@@ -1,7 +1,93 @@
-package db.migration /**
+package db.migration
+import org.flywaydb.core.api.migration.{BaseJavaMigration, Context}
+import org.json4s.Extraction.decompose
+import org.json4s.native.JsonMethods.{compact, parse, render}
+import org.json4s.{Formats, JArray, JValue}
+import org.postgresql.util.PGobject
+import scalikejdbc.{DB, DBSession, _}
+
+/**
   * Part of NDLA ndla.
   * Copyright (C) 2019 NDLA
   *
   * See LICENSE
   */
-class V18__RemoveDummyMetaDescription {}
+class V18__RemoveDummyMetaDescription extends BaseJavaMigration {
+  implicit val formats: Formats = org.json4s.DefaultFormats
+
+  override def migrate(context: Context): Unit = {
+    val db = DB(context.getConnection)
+    db.autoClose(false)
+
+    db.withinTx { implicit session =>
+      migrateArticles
+    }
+  }
+
+  def migrateArticles(implicit session: DBSession): Unit = {
+    val count = countAllArticles.get
+    var numPagesLeft = (count / 1000) + 1
+    var offset = 0L
+
+    while (numPagesLeft > 0) {
+      allArticles(offset * 1000).map {
+        case (id, document) => updateArticle(convertArticle(document), id)
+      }
+      numPagesLeft -= 1
+      offset += 1
+    }
+  }
+
+  def countAllArticles(implicit session: DBSession): Option[Long] = {
+    sql"""select count(*) from contentdata where document is not NULL"""
+      .map(rs => rs.long("count"))
+      .single()
+      .apply()
+  }
+
+  def allArticles(offset: Long)(implicit session: DBSession): Seq[(Long, String)] = {
+    sql"""
+         select id, document from contentdata
+         where document is not null
+         order by id limit 1000 offset $offset
+      """
+      .map(rs => {
+        (rs.long("id"), rs.string("document"))
+      })
+      .list
+      .apply()
+  }
+
+  def convertMetaDescription(metaDescription: List[V17__MetaDescription]): JValue = {
+    val newMetaDescriptions = metaDescription.map(meta => {
+      meta.content match {
+        case "Beskrivelse mangler" => V17__MetaDescription("", meta.language)
+        case _                     => V17__MetaDescription(meta.content, meta.language)
+      }
+    })
+    decompose(newMetaDescriptions)
+  }
+
+  def convertArticle(document: String): String = {
+    val oldArticle = parse(document)
+
+    val newArticle = oldArticle.mapField {
+      case ("metaDescription", metaDescription: JArray) =>
+        "metaDescription" -> convertMetaDescription(metaDescription.extract[List[V17__MetaDescription]])
+      case x => x
+    }
+    compact(render(newArticle))
+  }
+
+  private def updateArticle(document: String, id: Long)(implicit session: DBSession): Int = {
+    val dataObject = new PGobject()
+    dataObject.setType("jsonb")
+    dataObject.setValue(document)
+
+    sql"update contentdata set document = $dataObject where id = $id"
+      .update()
+      .apply
+  }
+
+  case class V17__MetaDescription(content: String, language: String)
+}
