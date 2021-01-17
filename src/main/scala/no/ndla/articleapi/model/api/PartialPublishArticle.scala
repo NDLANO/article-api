a885da60c78f9e57f2e3c7d53521b7f95716506a
N4G1 <bartezzzzz@hotmail.com>
2021-01-17 20:05:08 +0100

    extended partial publish method with license, tags and metaDescription

--- src/main/scala/no/ndla/articleapi/model/api/PartialPublishArticle.scala
+++ src/main/scala/no/ndla/articleapi/model/api/PartialPublishArticle.scala
@@ -15,5 +15,8 @@ import scala.annotation.meta.field
 // format: off
 @ApiModel(description = "Partial data about article to publish independently")
 case class PartialPublishArticle(
-    @(ApiModelProperty @field)(description = "A list of codes from GREP API connected to the article") grepCodes: Option[Seq[String]]
+    @(ApiModelProperty @field)(description = "A list of codes from GREP API connected to the article") grepCodes: Option[Seq[String]],
+    @(ApiModelProperty @field)(description = "The name of the license") license: Option[String],
+    @(ApiModelProperty @field)(description = "Meta description for the article") metaDescription: Option[String],
+    @(ApiModelProperty @field)(description = "A comma separated list of tags") tags: Option[Seq[String]],
 )
