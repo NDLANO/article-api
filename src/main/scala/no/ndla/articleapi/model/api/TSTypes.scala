3fbbc0ed4ff7c3c0f990e7378d7134de019bbbcc
N4G1 <bartezzzzz@hotmail.com>
2021-11-09 13:20:43 +0100

    added more detailed comments for TSTypes generation

--- src/main/scala/no/ndla/articleapi/model/api/TSTypes.scala
+++ src/main/scala/no/ndla/articleapi/model/api/TSTypes.scala
@@ -3,13 +3,10 @@ package no.ndla.articleapi.model.api
 import com.scalatsi._
 import no.ndla.articleapi.model.domain
 
-/**
-  * The `scala-tsi` plugin is not always able to derive the types that are used in `Seq` or other generic types.
-  * Therefore we need to explicitly load the case classes here.
-  * This is only necessary if the `sbt generateTypescript` script fails.
-  */
 object TSTypes {
+  // Type-aliases referencing generics doesn't not work without this in scala-tsi. See: https://github.com/scala-tsi/scala-tsi/issues/184
   implicit val relatedContent: TSIType[RelatedContentLink] = TSType.fromCaseClass[RelatedContentLink]
+  // Scala2 enumerations doesn't work as expected in scala-tsi. See: https://github.com/scala-tsi/scala-tsi/issues/182
   implicit val availability: TSType[domain.Availability.Value] =
     TSType.sameAs[domain.Availability.Value, domain.Availability.type]
 }
